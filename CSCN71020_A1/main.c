bdc055913ae9c734ef5488f0f773a0951a6c1556
Adityaparab123 <122999174+Adityaparab123@users.noreply.github.com>
2023-02-07 20:33:36 -0500

    added sub function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,7 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-
+void subtract();
 
 void main() {
 
@@ -20,6 +20,10 @@ void main() {
 	{
 	case 1:
 		add();
+		break;
+	case 2 :
+		subtract();
+		break;
 	}
 
 }
@@ -45,5 +49,17 @@ void add() {
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
 	result = num1 + num2+num3;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	printf("%lf + %lf + %lf = %lf\n", num1, num2, num2, result);
+}
+
+void subtract() {
+	double num1, num2, num3, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	printf("Enter the third value:");
+	scanf_s("%lf", &num3);
+	result = num1 - num2 - num3;
+	printf("%lf - %lf - %lf = %lf\n", num1, num2, num3, result);
 }
